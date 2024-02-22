ad725feb86eb4623232004e53f20354573a3356b
Jazz Cheema <jaskamalc@gmail.com>
2024-02-22 12:23:30 -0800

    on step 5 of pretzels

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ debug = DebugToolbarExtension(app)
 
 responses = []
 
+
 @app.get('/')
 def start_survey_page():
     """Returns home page with title and instructions for survey."""
@@ -18,6 +19,27 @@ def start_survey_page():
                            title=survey.title,
                            instructions=survey.instructions)
 
+
 @app.get('/questions/<int:question_id>')
 def question(question_id):
-    return render_template('question.html', question = survey.questions[question_id])
+    """captures id on questions and displays questions page"""
+    return render_template('question.html', question=survey.questions[question_id])
+
+
+@app.post('/answer')
+def answer_page():
+    """grabbing subbmited answers and redirecting to more questions."""
+
+    answers_from_form = request.form['answer']
+    responses.append(answers_from_form)
+
+    if len(responses) == len(survey.questions):
+        return redirect('/thankyou')
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.get('/thankyou')
+def thank_you_page():
+    """thank you page on completion of questions"""
+    return render_template('completion.html')
