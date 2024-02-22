2d64fadc3a2e21e589753e7766b096ed276c99f2
Anissa Nishioka <ani.nishioka@gmail.com>
2024-02-22 11:50:10 -0800

    implement part 1-3

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+
+responses = []
+
+@app.get('/')
+def start_survey_page():
+    """Returns home page with title and instructions for survey."""
+    return render_template('survey_start.html',
+                           title=survey.title,
+                           instructions=survey.instructions)
+
+@app.get('/questions/<int:question_id>')
+def question(question_id):
+    return render_template('question.html', question = survey.questions[question_id])
